983b234c0c67db2a006968b8be34ee9626d3fe38
Gleb-Vagin <vagingleb439@gmail.com>
2020-10-08 21:20:47 +0300

    Появилась возможность построить катру, но она корявая и  сохранить её нельзя

--- karta.py
+++ karta.py
@@ -1,5 +1,6 @@
 import pygame
 import settings
+import blocks
 
 print(__file__)
 # подготавливаем библиотеку
@@ -16,13 +17,25 @@ settings.SCREEN_WIDTH = screen.get_width()
 settings.SCREEN_HEIGHT = screen.get_height()
 pygame.display.set_caption("карта, просто карта")
 
+# СОЗДАЁМ КНОПКУ
+knopka = pygame.Rect(500, 400, 100, 50)
+pygame.draw.rect(screen, [228, 228, 228], knopka, 0)
+
 running = True
 while running:
     # задержка
     clock.tick(60)
+
     new_events = pygame.event.get()
     for event in new_events:
+
+        group_sprite = pygame.sprite.Group()
+
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            print("привет")
+            block = blocks.Block(event.pos[0], event.pos[1], blocks.BLOCK_TYPE_GROUND)
+            group_sprite.add(block)
+            group_sprite.draw(screen)
+
         if event.type == pygame.QUIT:
             running = False
+    pygame.display.flip()
